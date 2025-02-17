92a806d7656b0f3dd924bf89239766ae665c75ac
Oluwadarasimi Ayanlaja <daraayans@gmail.com>
2025-02-17 06:53:43 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/home'
+import Login from './pages/login'
+import ResetPassword from './pages/ResetPassword'
+import { ToastContainer, toast } from 'react-toastify';
+import EmailVerify from './pages/EmailVerify'
+import axios from "axios";
+
+
+const App = () => {
+
+  return (
+    <div>
+      <ToastContainer/>
+      <Routes>
+       <Route path='/' element={<Home/>}/>
+       <Route path='/login' element={<Login/>}/>
+       <Route path="/email-verify" element={<EmailVerify />} />
+       <Route path='/reset-password' element={<ResetPassword/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
